97ffd634032085f74dbee9546a183a1382eb2948
Sanjai <sanjaikumar.mani@gmail.com>
2023-03-11 02:58:19 +0530

    feat: updated water and central map

--- CentralMap/AbovePlayerLayer.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="AbovePlayerTiles" tilewidth="16" tileheight="16" tilecount="420" columns="14">
- <image source="AbovePlayerLayer.png" width="224" height="480"/>
-</tileset>
